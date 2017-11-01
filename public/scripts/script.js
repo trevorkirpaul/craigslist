d064de13950e3402313c2a0088dbc1d6cb6f6a85
Trevor Kirpaul <tkirpaul@gmail.com>
2017-11-01 12:00:17 -0400

    sell page form setup, media queries altered

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,6 +1,16 @@
+//mobile menu vars
+
 const btnMobileMore = document.getElementById('btnMobileNavMore');
 const menuMobileMore = document.getElementById('mobileNavMore');
 const btnCloseMobileMore = document.getElementById('btnMobileNavMoreBack');
+
+
+
+
+
+
+
+//mobile menu fxn
 
 btnMobileMore.addEventListener('click', (event) => {
   menuMobileMore.style.display = 'block';
